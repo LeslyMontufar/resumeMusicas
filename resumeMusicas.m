7587ffb21389b6d531ca8c3cd50e7ec20753178c
lesly <leslymontufarj@gmail.com>
2022-07-13 03:14:17 -0300

    funciona, tem requisito de músicas aleatórias

--- resumeMusicas.m
+++ resumeMusicas.m
@@ -1,16 +1,15 @@
-clc; clear variables; close all;
+clc; clear variables; close all; clear sound;
 fs = 44.1e3;        % frequência de amostragem
 nbits = 16;         % nro de bits da quantização do sinal
 ncanais = 1;        % nro de canais: mono, estéreo
 
-GRAVAR = 0;
-LER = 1;
+% GRAVAR = 0;
 MULTISELECT = 2;
 
 filename = 'sound1.mp4';
 duration = 60;      % gravar por 60 seg = 1 min
 startSec = 60;
-mode = LER;
+add = 5;
 selection_mode = MULTISELECT;
 
 secStart = startSec*fs;
@@ -20,29 +19,36 @@ if selection_mode == MULTISELECT
     [filename, path] = uigetfile('*', 'Select music', 'MultiSelect', 'on');
 end
 musicas = strcat(path, filename)';
+if ~iscell(musicas)
+    musicas = {musicas};
+end 
 
 for i = 1:length(musicas)
     filename = musicas{i,1};
-    if mode == GRAVAR
-        somObj = audiorecorder(fs,nbits,ncanais);
-        disp('Início ...');
-        recordblocking(somObj, duration); % gravar 
-        disp('Fim da gravação.');
-        dadosDouble = getaudiodata(somObj)';
-    elseif mode == LER
-        [dadosDouble,Fs] = audioread(filename, [secStart secStop]);          
-        dadosDouble = dadosDouble';
-    end
 
-    % if input("Ler gravação? (y/n)",'s')~='n'
-    if 1
-       if mode == GRAVAR
-           play(somObj); 
-       elseif mode == LER
-           disp("Tocando ...");
-           sound(dadosDouble,Fs,nbits);
-           disp("Fim.");
-       end
+    [dadosDouble,Fs] = audioread(filename);          
+    dadosDouble = dadosDouble';
+    
+    if (secStop>size(dadosDouble,2))
+        stop = size(dadosDouble,2);
+    else
+        stop = secStop;
     end
-    pause(duration);
+    
+    tic;
+    fprintf("Tocando ... ",i);
+    sound(dadosDouble(:,(secStart:stop)),Fs,nbits);
+    fprintf("a música %d\n",i);
+    
+    timer = toc;
+    ponto = add;
+    while (timer < duration) && (timer < stop/fs) 
+        timer = toc;
+        if(timer >= ponto)
+            fprintf('.',timer);
+            ponto=ponto+add;
+        end
+    end
+    clear sound;
+    fprintf("\nFim da música %d\n\n",i);
 end
